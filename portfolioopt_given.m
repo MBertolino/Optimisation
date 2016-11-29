e5e057084292de07456d4a885d65b278a668ea44
Jandren <andren.jakob@gmail.com>
2016-11-29 17:30:21 +0100

    med plot

--- portfolioopt_given.m
+++ portfolioopt_given.m
@@ -10,7 +10,7 @@ Aeq = [r;ones(size(r))];
 beq = [rho; 1];
 ub = ones(size(r))';
 lb = zeros(size(r))';
-lb = -ub;
+%lb = -ub;
 f = zeros(size(r));
 
 x0 = [];
@@ -18,4 +18,15 @@ x0 = [];
 options = optimoptions('quadprog','Algorithm','interior-point-convex');
 options = optimoptions(options,'Display','iter','TolCon', 1e-9,'TolFun',1e-10);
 
-[x,fval,exitflag] = quadprog(H,f,Aineq,bineq,Aeq,beq,lb,ub,x0,options);
+
+rho = 0.01:0.01:0.2;
+
+for ii = 1:length(rho)
+    beq = [rho(ii); 1];
+    [x,fval,exitflag] = quadprog(H,f,Aineq,bineq,Aeq,beq,lb,ub,x0,options);
+    
+    sigma(ii) = fval;
+    
+end
+
+plot(sigma, rho)
