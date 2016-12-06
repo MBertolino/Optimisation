841a52145564fe8a874fe3c624186e9341238128
Deurp <Hawk@student92-246.eduroam.uu.se>
2016-12-06 13:28:46 +0100

    Merge remote-tracking branch 'MBertolino/master'
    # Conflicts:
    #	portfolioopt_given.m
    #	portfolioopt_given_changes.m

---
+++ Ass3_linprog.m
@@ -0,0 +1,60 @@
+clear all
+
+% Object function
+C = [10, 0, 20, 11, 12, 7, 9, 20, 0, 14, 16, 18]; 
+
+% Supply constraint
+Ain = blkdiag(ones(1,4), ones(1,4), ones(1,4));
+supply = [25; 55; 35];
+% S2 = 60, Q4
+supply2 = [25; 60; 35];
+
+% Demand constraint
+Aeq = [eye(4), eye(4), eye(4)];
+demand = [15; 45; 30; 25];
+
+
+%% Interior point
+options = optimoptions('linprog','Algorithm','interior-point', 'Display', 'iter');
+
+  x = linprog(C, Ain, supply, Aeq, demand, zeros(12,1), [], [], options);
+  
+  Ain*x;
+  Aeq*x;
+  
+  
+  x2 = linprog(C, Ain, supply2, Aeq, demand, zeros(12,1), [], [], options);
+  
+  Ain*x2;
+  Aeq*x2;
+  
+%% Dual simplex
+  options = optimoptions('linprog','Algorithm','dual-simplex', 'Display', 'iter');
+
+  x3 = linprog(C, Ain, supply, Aeq, demand, zeros(12,1), [], [], options);
+  
+  Ain*x;
+  Aeq*x;
+  
+  % S2 = 60, Q4
+  supply = [25; 60; 35];
+  x4 = linprog(C, Ain, supply2, Aeq, demand, zeros(12,1), [], [], options);
+  
+  Ain*x2;
+  Aeq*x2;
+  
+  %% Results
+  X = [x, x3, x2, x4]
+  
+  Used = Ain*X
+  subplot(121)
+
+  bar3(reshape(C, 4,3)')
+  title('Fraktkostnad')
+  xlabel('Varuhus')
+  ylabel('Fabrik')
+  subplot(122)
+    bar3(reshape(x,4,3)')
+  title('Fraktade varor')
+    xlabel('Varuhus')
+  ylabel('Fabrik')
--- portfolioopt_given.m
+++ portfolioopt_given.m
@@ -5,6 +5,7 @@ data_input
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 % Quadprog to minimise std given expected return
 rho = 0.1; % Seeked return
 Aineq = []; % Olikheter, 0 för oss
@@ -18,6 +19,8 @@ f = zeros(size(r)); % Nothing
 
 x0 = []; % Startvärde? - Interior point convex algoritm slumpar startvärde.
 =======
+=======
+>>>>>>> MBertolino/master
 =======
 >>>>>>> MBertolino/master
 %% 2. KKT system. Risk avert
@@ -65,6 +68,7 @@ sigma2_b = w3b'*H*w3b;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 rho = 0.01:0.01:0.2; % Different rate of return conditions
 
 for ii = 1:length(rho)
@@ -87,11 +91,17 @@ sigma2_c = w3c'*H*w3c;
 w3c = quadprog(H,f,[],[],Aeq_c,beq_c,lb,ub,x0,options);
 sigma2_c = w3c'*H*w3c;
 >>>>>>> MBertolino/master
+=======
+% c)
+w3c = quadprog(H,f,[],[],Aeq_c,beq_c,lb,ub,x0,options);
+sigma2_c = w3c'*H*w3c;
+>>>>>>> MBertolino/master
 
 %% 4. Efficient frontier, stepping in alpha
 Aeq = ones(size(r));
 beq = 1;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 alpha = 0.05:0.05:1;
@@ -138,6 +148,20 @@ for ai = 1:length(alpha)
 end
 
 >>>>>>> MBertolino/master
+=======
+alpha = 0:0.05:1;
+w4 = zeros(length(alpha),length(r));
+sigma_ef = zeros(length(alpha),1);
+ret_ef = sigma_ef;
+for ai = 1:length(alpha)
+    Q = 2*alpha(ai)*H;
+    f = (alpha(ai)-1)*r';
+    [w4(ai,:),fval,exitflag] = quadprog(Q,f,[],[],Aeq,beq,lb,ub,[],options);
+    sigma_ef(ai) = sqrt(w4(ai,:)*H*w4(ai,:)');
+    ret_ef(ai) = r*w4(ai,:)';
+end
+
+>>>>>>> MBertolino/master
 % Plot efficient frontier
 figure()
 subplot(2,1,1)
@@ -150,6 +174,9 @@ legend('Individual assets', 'Efficient frontier')
 xlim([0.05 max(stdev)*1.1])
 ylim([0 max(r)*1.2])
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> MBertolino/master
+=======
 >>>>>>> MBertolino/master
 =======
 >>>>>>> MBertolino/master
